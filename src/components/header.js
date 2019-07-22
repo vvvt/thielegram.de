dec0fc3b54c447e74bc5e6e06b850753f565ba6d
Vincent Thiele <vincent@thielegram.de>
2019-07-22 10:31:50 +0200

    :wheelchair: ADD roles to navigation

--- src/components/header.js
+++ src/components/header.js
@@ -4,12 +4,20 @@ import React from "react"
 
 const Header = ({ siteTitle }) => (
   <header>
-    <nav>
-      <ul>
-        <li><Link to="/" class="nav-item" activeClassName="nav-item nav-active">Thielegram</Link></li>
-        <li><Link to="/code" class="nav-item" activeClassName="nav-item nav-active">Code</Link></li>
-        <li><Link to="/music" class="nav-item" activeClassName="nav-item nav-active">Music</Link></li>
-        <li><Link to="/art" class="nav-item" activeClassName="nav-item nav-active">Art</Link></li>
+    <nav aria-label="Main">
+      <ul role="menubar">
+        <li role="presentation">
+          <Link to="/" role="menuitem" aria-label="Home" class="nav-item" activeClassName="nav-item nav-active">Thielegram</Link>
+          </li>
+        <li role="presentation">
+          <Link to="/code" role="menuitem" aria-label="Code" class="nav-item" activeClassName="nav-item nav-active">Code</Link>
+        </li>
+        <li role="presentation">
+          <Link to="/music" role="menuitem" aria-label="Music" class="nav-item" activeClassName="nav-item nav-active">Music</Link>
+        </li>
+        <li role="presentation">
+          <Link to="/art" role="menuitem" aria-label="Art" class="nav-item" activeClassName="nav-item nav-active">Art</Link>
+        </li>
       </ul>
     </nav>
   </header>
