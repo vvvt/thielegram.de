f883522a7b0b481fba18b32a980a1c996d20deee
Vincent Thiele <vincent@thielegram.de>
2019-07-23 16:01:14 +0200

    :fire: REMOVE unused statements

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,12 +5,12 @@ import github from '../images/icon-github.svg'
 import mail from '../images/icon-mail.svg'
 import telegram from '../images/icon-telegram.svg'
 
-const Footer = (props) => (
+const Footer = () => (
   <footer>
     <span>© {new Date().getFullYear()} - Vincent Thiele - <Link to="/imprint">Impressum</Link></span>
-    <a class="social" aria-label="Github Profile" href="https://www.github.com/vvvt"><img class="social-img" role="presentation" alt="Github Icon" src={github} /></a>
-    <a class="social" aria-label="Telegram Contact" href="https://t.me/thielegram"><img class="social-img" role="presentation" alt="Telegram Icon" src={telegram} /></a>
-    <a class="social" aria-label="Mail Contact" href="mailto:hey@thielegram.de"><img class="social-img" role="presentation" alt="E-Mail Icon" src={mail} /></a>    
+    <a class="social" aria-label="Github Profile" href="https://www.github.com/vvvt"><img class="social-img" aria-hidden="true" alt="Github Icon" src={github} /></a>
+    <a class="social" aria-label="Telegram Contact" href="https://t.me/thielegram"><img class="social-img" aria-hidden="true" alt="Telegram Icon" src={telegram} /></a>
+    <a class="social" aria-label="Mail Contact" href="mailto:hey@thielegram.de"><img class="social-img" aria-hidden="true" alt="E-Mail Icon" src={mail} /></a>    
   </footer>
 )
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,5 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
@@ -15,21 +7,11 @@ import "./normalize.css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header/>
       <main>{children}</main>
-      <Footer />
+      <Footer/>
     </>
   )
 }
