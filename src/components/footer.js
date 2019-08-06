f8c73c11bb9a6db99bbb10c89bcd38d91dec34b5
Vincent Thiele <vincent@thielegram.de>
2019-08-07 01:21:52 +0200

    :lipstick: IMPORT new social icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import github from '../images/icons8-octocat-50.png'
-import mail from '../images/icons8-important-mail-50.png'
-import telegram from '../images/icons8-telegram-app-50.png'
-import icons8 from '../images/icons8-50.png'
+import github from '../images/icon-github2.svg'
+import mail from '../images/icon-mail2.svg'
+import telegram from '../images/icon-telegram2.svg'
 
 const Footer = () => (
   <footer>
@@ -13,7 +12,6 @@ const Footer = () => (
       <a aria-label="Github Profile" href="https://www.github.com/vvvt"><img class="social-img" aria-hidden="true" alt="Github Icon" src={github} /></a>
       <a aria-label="Telegram Contact" href="https://t.me/thielegram"><img class="social-img" aria-hidden="true" alt="Telegram Icon" src={telegram} /></a>
       <a aria-label="Mail Contact" href="mailto:hey@thielegram.de"><img class="social-img" aria-hidden="true" alt="E-Mail Icon" src={mail} /></a> 
-      <a aria-label="Icons made by" href="https://icons8.com/"><img class="social-img" aria-hidden="true" alt="Icons8 Icon" src={icons8} /></a>    
     </div>
   </footer>
 )
