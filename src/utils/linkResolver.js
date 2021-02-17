2ec908287a3cd0482d7d304babfda74dcc1c6349
Vincent Thiele <vincent_julius.thiele@mailbox.tu-dresden.de>
2021-02-17 10:10:24 +0100

    EDIT formatting

--- src/utils/linkResolver.js
+++ src/utils/linkResolver.js
@@ -3,11 +3,11 @@
 // As your project grows, you should update this function according to your routes
 
 exports.linkResolver = function linkResolver(doc) {
-    // Route for blog posts
-    if (doc.type === 'Post') {
-        return '/blog/' + doc.uid;
-    }
+  // Route for blog posts
+  if (doc.type === "Post") {
+    return "/blog/" + doc.uid
+  }
 
-    // Homepage route fallback
-    return '/';
+  // Homepage route fallback
+  return "/"
 }
